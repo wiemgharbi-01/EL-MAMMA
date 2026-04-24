336ed25d4c9b0fc78c2edbc932415a903e2d1d85
Hazem <hazem.benibrahim@etudiant-fsegt.utm.tn>
2026-04-24 22:54:55 +0100

    Java

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        plat p = new plat("Pâtes maison", 2);
+
+        p.reserverPlat();
+        p.reserverPlat();
+        p.reserverPlat();
+    }
+}
