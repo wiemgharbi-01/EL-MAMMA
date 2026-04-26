899aaaa37bbe92ce7987bf9b2e8879e2bddc5e8c
isseffj-a11y <isseffj@gmail.com>
2026-04-26 15:45:11 +0100

    Suppression des fichiers de test temporaires.

--- src/main1.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        plat p = new plat("Pâtes maison", 2);
-
-        p.reserverPlat();
-        p.reserverPlat();
-        p.reserverPlat();
-    }
-}
